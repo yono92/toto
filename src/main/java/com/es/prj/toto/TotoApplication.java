980a66f81da79f6050078323b88096c3248de45c
yoonsejong <oxwk123@gmail.com>
2022-12-10 10:40:45 +0900

    first commit

---
+++ src/main/java/com/es/prj/toto/TotoApplication.java
@@ -0,0 +1,13 @@
+package com.es.prj.toto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TotoApplication.class, args);
+	}
+
+}

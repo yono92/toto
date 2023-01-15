69308eef49d1e7314e6a08a49535841fbec1eb80
gimtaegang <xorkd2951@naver.com>
2023-01-15 22:07:36 +0900

    spring security + jwt 추가

--- src/test/java/com/es/prj/toto/util/UtilTest.java
+++ src/test/java/com/es/prj/toto/util/UtilTest.java
@@ -1,5 +1,6 @@
 package com.es.prj.toto.util;
 
+import com.es.prj.toto.common.util.EncryptUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
